41273071de842497ed39a0cfe58c9533353535ef
Polina Makarova <pomakorel@gmail.com>
2022-01-18 17:21:33 +0300

    test

--- tests/genDiff.test.js
+++ tests/genDiff.test.js
@@ -1,3 +1,9 @@
-import assert from 'power-assert';;
-import genDiff from "../src/genDiff.js";
+import genDiff from '../src/genDiff.js';
+import assert from 'power-assert';
 
+const difference = genDiff();
+const rightResult = 
+
+test('get right difference', () => {
+  expect(difference().toEqual());
+})
