f5df363087c2ccd6a496bc28d4da66a536a57686
Polina Makarova <pomakorel@gmail.com>
2022-03-11 15:40:22 +0300

    parser

--- src/genDiff.js
+++ src/genDiff.js
@@ -1,11 +1,14 @@
+import { readFileSync } from 'fs';
 import path from 'path';
 import parse from './parser.js';
 import buildAst from './buildAst.js';
 import format from './formatters/index.js';
 
 const genDiff = (file1, file2, formatter) => {
-  const objectForFile1 = parse(file1, path.extname(file1));
-  const objectForFile2 = parse(file2, path.extname(file2));
+  const content1 = readFileSync(path.resolve(file1));
+  const content2 = readFileSync(path.resolve(file2));
+  const objectForFile1 = parse(content1, path.extname(file1).slice(1));
+  const objectForFile2 = parse(content2, path.extname(file2).slice(1));
 
   const ast = buildAst(objectForFile1, objectForFile2);
   return format(ast, formatter);
--- src/parser.js
+++ src/parser.js
@@ -1,15 +1,13 @@
-import { readFileSync } from 'fs';
-import path from 'path';
 import yaml from 'js-yaml';
 
-const parse = (file) => {
-  switch (path.extname(file)) {
-    case '.json':
-      return JSON.parse(readFileSync(path.resolve(file)));
-    case '.yml':
-      return yaml.load(readFileSync(path.resolve(file)));
-    case '.yaml':
-      return yaml.load(readFileSync(path.resolve(file)));
+const parse = (content, format) => {
+  switch (format) {
+    case 'json':
+      return JSON.parse(content);
+    case 'yml':
+      return yaml.load(content);
+    case 'yaml':
+      return yaml.load(content);
     default:
       return {};
   }
